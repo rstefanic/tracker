011c7d7c011f7653ed8f40b07c97e6196dfe0baa
Robert Stefanic <rstefanic72@gmail.com>
2024-05-05 20:33:32 -0400

    main: Handle Unix Signals for quitting
    This'll be used later to handle some clean up before quitting the
    program.
    
    Additionally: Pulled all of the code out of main into `runTracker`. This
    function has the same signature as the argument for `macos.RunApp`
    allowing us to pull everything out one indentation level.

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"tracker/display"
 	"tracker/tracker"
 
@@ -10,18 +14,29 @@ import (
 )
 
 func main() {
+	macos.RunApp(runTracker)
+}
+
+func runTracker(a appkit.Application, ad *appkit.ApplicationDelegate) {
+	go handleExitSignal()
+
 	tracker := tracker.NewTracker()
 	display := display.NewDisplay()
 
-	macos.RunApp(func(a appkit.Application, ad *appkit.ApplicationDelegate) {
-		var workspace appkit.Workspace = appkit.Workspace_SharedWorkspace()
-		var oneSec foundation.TimeInterval = 1.0
-		var repeat bool = true
+	var workspace appkit.Workspace = appkit.Workspace_SharedWorkspace()
+	var oneSec foundation.TimeInterval = 1.0
+	var repeat bool = true
 
-		foundation.Timer_ScheduledTimerWithTimeIntervalRepeatsBlock(oneSec, repeat, func(_ foundation.Timer) {
-			app := workspace.FrontmostApplication()
-			tracker.RecordUsage(app.LocalizedName())
-			display.PrintUsage(tracker.Usage)
-		})
+	foundation.Timer_ScheduledTimerWithTimeIntervalRepeatsBlock(oneSec, repeat, func(_ foundation.Timer) {
+		app := workspace.FrontmostApplication()
+		tracker.RecordUsage(app.LocalizedName())
+		display.PrintUsage(tracker.Usage)
 	})
+}
+
+func handleExitSignal() {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+	os.Exit(0)
 }
